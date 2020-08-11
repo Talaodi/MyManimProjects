8323c45985d7f66ab401969dd8b54debbcbd8e12
“gTalaodi <great.talaodi@gmail.com>
2020-08-11 17:32:14 +0800

    Format code.

--- Exercises/Differential.py
+++ Exercises/Differential.py
@@ -1,5 +1,6 @@
 from manimlib.imports import *
 
+
 class Differential(GraphScene):
     CONFIG = {
         "x_min": -6,
@@ -17,18 +18,24 @@ class Differential(GraphScene):
         self.play(ShowCreation(self.square_graph))
         self.wait(2)
 
-        start_point = Dot(self.input_to_graph_point(1, self.square_graph)).set_color(BLUE)
-        end_point = Dot(self.input_to_graph_point(3, self.square_graph)).set_color(BLUE)
-        start_line = Line(self.coords_to_point(1, 0), self.coords_to_point(1, 0)).set_color(YELLOW)
-        end_line = Line(self.coords_to_point(3, 0), self.coords_to_point(3, 0)).set_color(YELLOW)
+        start_point = Dot(self.input_to_graph_point(
+            1, self.square_graph)).set_color(BLUE)
+        end_point = Dot(self.input_to_graph_point(
+            3, self.square_graph)).set_color(BLUE)
+        start_line = Line(self.coords_to_point(
+            1, 0), self.coords_to_point(1, 0)).set_color(YELLOW)
+        end_line = Line(self.coords_to_point(3, 0),
+                        self.coords_to_point(3, 0)).set_color(YELLOW)
         line = Line(start_point.get_center(), end_point.get_center())
         line.set_length(8)
         self.add(VGroup(start_line, end_line))
 
         self.play(
             ShowCreation(VGroup(start_point, end_point)),
-            start_line.put_start_and_end_on, self.coords_to_point(1, 0), start_point.get_center(),
-            end_line.put_start_and_end_on, self.coords_to_point(3, 0), end_point.get_center()
+            start_line.put_start_and_end_on, self.coords_to_point(
+                1, 0), start_point.get_center(),
+            end_line.put_start_and_end_on, self.coords_to_point(
+                3, 0), end_point.get_center()
         )
 
         self.wait(1)
@@ -36,17 +43,20 @@ class Differential(GraphScene):
         start_x = ValueTracker(1)
         end_x = ValueTracker(3)
         start_point.add_updater(
-            lambda m: m.move_to(self.input_to_graph_point(start_x.get_value(), self.square_graph))
+            lambda m: m.move_to(self.input_to_graph_point(
+                start_x.get_value(), self.square_graph))
         )
         end_point.add_updater(
-            lambda m: m.move_to(self.input_to_graph_point(end_x.get_value(), self.square_graph))
+            lambda m: m.move_to(self.input_to_graph_point(
+                end_x.get_value(), self.square_graph))
         )
 
         def line_move(mobject: Line):
             if np.all(start_point.get_center() == end_point.get_center()):
                 return mobject
 
-            mobject.put_start_and_end_on(start_point.get_center(), end_point.get_center())
+            mobject.put_start_and_end_on(
+                start_point.get_center(), end_point.get_center())
             mobject.set_length(8)
             return mobject
 
@@ -59,14 +69,18 @@ class Differential(GraphScene):
             run_time=4
         )
 
-        text_x_eq_2 = TexMobject("x=2").next_to(self.coords_to_point(2, 0), DOWN)
-        text_slope = TextMobject("Slope =", "4").scale(1.25).next_to(text_x_eq_2, DOWN)
+        text_x_eq_2 = TexMobject("x=2").next_to(
+            self.coords_to_point(2, 0), DOWN)
+        text_slope = TextMobject("Slope =", "4").scale(
+            1.25).next_to(text_x_eq_2, DOWN)
         text_2x = TexMobject("2x").scale(1.25).next_to(text_slope[0], RIGHT)
-        mid_line = Line(self.coords_to_point(2, 4), self.coords_to_point(2, 4)).set_color(YELLOW)
+        mid_line = Line(self.coords_to_point(2, 4),
+                        self.coords_to_point(2, 4)).set_color(YELLOW)
 
         self.play(
             Write(text_x_eq_2),
-            mid_line.put_start_and_end_on, self.coords_to_point(2, 4), self.coords_to_point(2, 0)
+            mid_line.put_start_and_end_on, self.coords_to_point(
+                2, 4), self.coords_to_point(2, 0)
         )
         self.play(Write(text_slope), run_time=2)
         self.wait(1)
--- Exercises/Integral.py
+++ Exercises/Integral.py
@@ -18,8 +18,10 @@ class Integral(GraphScene):
         self.play(ShowCreation(self.square_graph))
         self.wait(2)
 
-        start_line = Line(self.input_to_graph_point(1, self.square_graph), self.coords_to_point(1, 0), color=YELLOW)
-        end_line = Line(self.input_to_graph_point(3, self.square_graph), self.coords_to_point(3, 0), color=YELLOW)
+        start_line = Line(self.input_to_graph_point(
+            1, self.square_graph), self.coords_to_point(1, 0), color=YELLOW)
+        end_line = Line(self.input_to_graph_point(
+            3, self.square_graph), self.coords_to_point(3, 0), color=YELLOW)
         text_start = TexMobject("1").next_to(start_line, DOWN)
         text_end = TexMobject("3").next_to(end_line, DOWN)
         self.play(Write(VGroup(start_line, end_line, text_start, text_end)))
@@ -27,15 +29,18 @@ class Integral(GraphScene):
 
         numbers = [3, 5, 10, 30, 60, 100]
         last = self.make_rectangles(1, 3)
-        text_number = TextMobject("Number:1").next_to(VGroup(text_start, text_end), DOWN)
+        text_number = TextMobject("Number:1").next_to(
+            VGroup(text_start, text_end), DOWN)
         text_dx = TextMobject("d$x$:2").next_to(text_number, DOWN)
         self.play(Write(VGroup(last, text_number, text_dx)))
         self.wait(1)
 
         for number in numbers:
             now = self.make_rectangles(1, 3, number)
-            text_new_number = TexMobject(str(number)).next_to(text_number[0][6], RIGHT)
-            text_new_dx = TexMobject("%.2f" % (2 / number)).next_to(text_dx[0][2], RIGHT)
+            text_new_number = TexMobject(
+                str(number)).next_to(text_number[0][6], RIGHT)
+            text_new_dx = TexMobject(
+                "%.2f" % (2 / number)).next_to(text_dx[0][2], RIGHT)
             self.play(
                 Transform(last, now),
                 Transform(text_number[0][7], text_new_number),
@@ -44,8 +49,10 @@ class Integral(GraphScene):
             self.wait(0.2)
 
         self.wait(1)
-        text_sum = TexMobject("\\sum_i f(x_i){\\rm d}x \\approx %.2f" % (9 - 0.33)).next_to(text_dx, DOWN)
-        text_int = TexMobject("\\int_1^3 f(x){\\rm d}x=\\frac{26}{3}").move_to(text_sum)
+        text_sum = TexMobject("\\sum_i f(x_i){\\rm d}x \\approx %.2f" % (
+            9 - 0.33)).next_to(text_dx, DOWN)
+        text_int = TexMobject(
+            "\\int_1^3 f(x){\\rm d}x=\\frac{26}{3}").move_to(text_sum)
         self.play(Write(text_sum))
         self.wait(1)
         self.play(Transform(text_sum, text_int))
@@ -53,7 +60,8 @@ class Integral(GraphScene):
         self.wait(10)
 
     def make_rectangles(self, start, end, number=1):
-        width = (self.coords_to_point(end, 0)[0] - self.coords_to_point(start, 0)[0]) / number
+        width = (self.coords_to_point(end, 0)[
+                 0] - self.coords_to_point(start, 0)[0]) / number
         size = (end - start) / number
         rectangles = VGroup(*(
             Rectangle(
